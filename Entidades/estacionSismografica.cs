6b1e4131a1f929cb91e4c4cd3e68377b0d682272
Gaston Capellan <gastoncapellan@gmail.com>
2025-06-03 08:20:42 -0300

    Update estacionSismografica.cs
    atributos privados

--- Entidades/estacionSismografica.cs
+++ Entidades/estacionSismografica.cs
@@ -8,19 +8,19 @@ namespace PPAI_REDSISMICA.Entidades
 {
     public class EstacionSismografica
     {
-        public int codigoEstacion { get; set; }
+        private int codigoEstacion { get; set; }
 
-        public string documentoCertificacionAdq { get; set; }
+        private string documentoCertificacionAdq { get; set; }
 
-        public DateTime fechaSolicitudCertificacion { get; set; }
+        private DateTime fechaSolicitudCertificacion { get; set; }
 
-        public double latitud { get; set; }
+        private double latitud { get; set; }
 
-        public double longitud { get; set; }   
+        private double longitud { get; set; }   
 
-        public string nombre { get; set; } 
+        private string nombre { get; set; } 
 
-        public int nroCerticacionAdquisicion { get; set; }  
+        private int nroCerticacionAdquisicion { get; set; }  
 
 
         public EstacionSismografica(int codigoEstacion, string documentoCertificacionAdq, DateTime fechaSolicitudCertificacion, double latitud, double longitud, string nombre, int nroCerticacionAdquisicion)
