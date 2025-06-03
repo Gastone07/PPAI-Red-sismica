0822eff5beb4b2f07e775314486d311b5dc8b1b9
Gaston Capellan <gastoncapellan@gmail.com>
2025-06-03 08:19:20 -0300

    Update Sesion.cs
    atributos privados

--- Entidades/Sesion.cs
+++ Entidades/Sesion.cs
@@ -8,23 +8,16 @@ namespace PPAI_REDSISMICA.Entidades
 {
     public class Sesion
     {
-        public Usuario usuario { get; set; }
-        public DateTime fechaHoraInicio { get; set; }
-        public DateTime? fechaHoraFin { get; set; }
+        private Usuario usuario { get; set; }
+        private DateTime fechaHoraInicio { get; set; }
+        private DateTime? fechaHoraFin { get; set; }
+        
         public Sesion(Usuario usuario, DateTime fechaHoraInicio, DateTime? fechaHoraFin)
         {
             this.usuario = usuario;
             this.fechaHoraInicio = fechaHoraInicio;
             this.fechaHoraFin = fechaHoraFin;
         }
-
-        /*
-        public Sesion(Usuario usuario, DateTime fechaHoraInicio)
-        {
-            this.usuario = usuario;
-            this.fechaHoraInicio = fechaHoraInicio;
-            this.fechaHoraFin = null;
-        }*/
 
         public static Usuario obtenerUsuarioLogueado(List<Sesion> listadoSesiones)
         {
