a3c8031f6e092951a209a6b4b677c08296a80efa
Gaston Capellan <gastoncapellan@gmail.com>
2025-06-03 08:23:12 -0300

    Update tipoDeDato.cs

--- Entidades/tipoDeDato.cs
+++ Entidades/tipoDeDato.cs
@@ -8,11 +8,11 @@ namespace PPAI_REDSISMICA.Entidades
 {
     public class TipoDeDato
     {
-        public string denominacion { get; set; }
+        private string denominacion { get; set; }
 
-        public string nombreUnidadMedida { get; set; }
+        private string nombreUnidadMedida { get; set; }
 
-        public double valorUmbral { get; set; }
+        private double valorUmbral { get; set; }
 
         public TipoDeDato(string denominacion, string nombreUnidadMedida, double valorUmbral)
         {
